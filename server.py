f6fc04607f3be6564235f0237ac88ed32482ba95
dragonleg <mis.bloko@gmail.com>
2017-04-16 12:24:16 +0200

    Add 404 page

--- server.py
+++ server.py
@@ -1,15 +1,21 @@
-from flask import Flask, render_template
-from utils import get_pages, get_thread_urls, get_pictures_urls, BOARD
+from flask import Flask, render_template, abort
+from utils import get_pages, get_thread_urls, get_pictures_urls
 app = Flask(__name__)
 
 
-@app.route('/')
-def show_threads():
-    urls = get_thread_urls(get_pages())
-    return render_template('home.html', urls=urls, board=BOARD)
+@app.route('/<string:board>/')
+def show_threads(board):
+    urls = get_thread_urls(get_pages(board))
+    if urls is not None:
+        return render_template('home.html', urls=urls, board=board)
+    else:
+        return render_template('404.html', board=board)
 
 
-@app.route('/thread/<int:thread>')
-def thread_page(thread):
-    urls = get_pictures_urls(thread)
-    return render_template('thread.html', board=BOARD, urls=urls)
+@app.route('/<string:board>/thread/<int:thread>')
+def thread_page(board, thread):
+    urls = get_pictures_urls(thread, board)
+    if urls is not None:
+        return render_template('thread.html', board=board, urls=urls)
+    else:
+        return render_template('404.html', thread=thread)
--- utils.py
+++ utils.py
@@ -1,34 +1,46 @@
 import requests
 
-BOARD = 'wg'
-
-API_BOARD = 'http://a.4cdn.org/{0}/catalog.json'.format(BOARD)
+API_BOARD = 'http://a.4cdn.org/{0}/catalog.json'
 API_THREAD = 'http://a.4cdn.org/{0}/thread/{1}.json'
 
 IMG_SOURCE = 'http://i.4cdn.org/{0}/{1}'
 
 
 def send_request(url):
-    r = requests.get(url)
-    return r.json()
+    try:
+        r = requests.get(url)
+        return r.json()
+    except ValueError:
+        return None
 
 
-def get_pages(page=None):
-    json = send_request(API_BOARD)
-    print len(json)
-    if page is None:
-        return [page for page in json]
-    return json[page - 1]['threads']
+def get_pages(board, page=None):
+    json = send_request(API_BOARD.format(board))
+    if json is not None:
+        print len(json)
+        if page is None:
+            return [page for page in json]
+        return json[page - 1]['threads']
+    else:
+        return None
 
 
 def get_thread_urls(pages):
-    urls = [str(thread['no']) for page in pages for thread in page['threads']]
-    return urls
+    if pages is not None:
+        urls = [str(thread['no']) for page in pages
+                for thread in page['threads']]
+        return urls
+    else:
+        return None
 
 
-def get_pictures_urls(thread):
-    json = send_request(API_THREAD.format(BOARD, str(thread)))
-    urls = [IMG_SOURCE.format(BOARD, str(post['tim']) + post['ext'])
-            for post in json['posts']if 'tim' in post
-            if 'ext' in post if post['w'] > 700]
-    return urls
+def get_pictures_urls(thread, board, min_pic_width=0, min_pic_height=0):
+    json = send_request(API_THREAD.format(board, str(thread)))
+    if json is not None:
+        urls = [IMG_SOURCE.format(board, str(post['tim']) + post['ext'])
+                for post in json['posts'] if 'tim' in post
+                if 'ext' in post if post['w'] > min_pic_width
+                if post['h'] > min_pic_height]
+        return urls
+    else:
+        return None
